b0a3ba6ac38b35366128f6fbe4bffbeefecfc2ec
youchenlong <54472586+youchenlong@users.noreply.github.com>
2022-06-27 18:00:20 +0800

    Add files via upload

--- Flask后端/db_config.py
+++ Flask后端/db_config.py
@@ -5,6 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # 数据库配置
+# 协议 mysql+pymysql
+# 用户名 root（用自己的用户名）
+# 密码 root（用自己的密码）
+# IP地址 localhost
+# 端口 3306
+# 数据库名 wechat
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost:3306/wechat"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db_init = SQLAlchemy(app)
